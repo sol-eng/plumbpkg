a77e7b92c457919c41f30ad5f27898eeabe9e9fb
James Blair <james.m.blair09@gmail.com>
2019-09-10 22:10:33 -0600

    Add run_api2() exported function

--- R/api2.R
+++ R/api2.R
@@ -1,8 +1,22 @@
 delay <- function(max_s = 1, fail = 0, req, res) {
   max_s <- as.numeric(max_s)
-  s <- runif(1, max = max_s)
+  s <- stats::runif(1, max = max_s)
   Sys.sleep(s)
   # Randomly fail certain responses
-  res$status <- if(runif(1) <= as.numeric(fail)) 404 else 200
+  res$status <- if(stats::runif(1) <= as.numeric(fail)) 404 else 200
   list(msg = paste0("Slept for ", s, " seconds"))
+}
+
+#' Run API 2
+#'
+#' @param ... Options passed to \code{plumber::plumb()$run()}
+#' @examples
+#' \dontrun{
+#' run_api2()
+#' run_api2(swagger = TRUE, port = 8000)
+#' }
+#' @return A running Plumber API
+#' @export
+run_api2 <- function(...) {
+  plumber::plumb(dir = system.file("plumber", "api2", package = "plumbpkg"))$run(...)
 }
